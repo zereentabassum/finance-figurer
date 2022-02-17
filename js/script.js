ff636ccaf92e5f117429d9aa5d3822d133eb2e2e
zereentabassum <islamms262@gmail.com>
2022-02-17 21:51:44 +0600

    js bonus part added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ const clothesValue = document.getElementById('clothes-value');
 let totalExpenses = document.getElementById('total-expenses');
 const balance = document.getElementById('balance');
 
+// Saving 
+const savingInput = document.getElementById('saving-input');
+
+// Saving Amount and Remaining Balance 
+const savingAmount = document.getElementById('saving-amount');
+const remainingBalance = document.getElementById('remaining-balance');
+
 
 //---- total expenses calculation function----
 function expenseCalculate(foodValue, rentValue, clothesValue){
@@ -30,19 +37,50 @@ function expenseCalculate(foodValue, rentValue, clothesValue){
    clothesValue.value = '';
 }
 }
+
 //------- Balance and Remaining Balance calculation function-------
 function remainingMoney(firstValue, secondValue) {
     return Number(secondValue - firstValue);
 }
 
+ //------ Saving Calculation 
+function savingsCalculation(incomeValue, savingInput) {
+    savingAmount.innerText = Number(incomeValue.value) * Number(savingInput.value)/100;
+    savingInput.value = '';
+}
 
-//------ Calculate Button click events 
+
+//------ ---Calculate Button click events -----------
 // Total Expenses 
 document.getElementById('calculate').addEventListener('click', function() {
   expenseCalculate(foodValue, rentValue, clothesValue);
   
 //   Balance
+if (Number(totalExpenses.innerText) > Number(incomeValue.value)) {
+    let failError = document.getElementById('fail-error');
+    failError.style.display = 'block';
+}
+else {
  const balanceOutput = remainingMoney(totalExpenses.innerText, incomeValue.value);
  balance.innerText = balanceOutput;
- incomeValue.value = '';
+}
+})
+
+
+//---------- Save Button Click Events --------------
+document.getElementById('save-button').addEventListener('click', function() {
+     // savingAmount 
+    if (Number(savingAmount.innerText) > Number(balance.innerText) ) {
+        let errorAlert = document.getElementById('error-alert');
+        errorAlert.style.display = 'block';
+    }
+    else {
+    savingsCalculation(incomeValue, savingInput);
+    
+    // Remaining Balance 
+    const balanceRemained = remainingMoney(savingAmount.innerText, balance.innerText);
+    remainingBalance.innerText = balanceRemained;
+    savingInput = '';
+    incomeValue = '';
+    }
 })
